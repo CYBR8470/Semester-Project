1b81035e212d97b7bdf6f67454d75c54a65974fb
Pat <pspeters93@gmail.com>
2021-11-29 22:44:34 -0600

    added game model and migrated

--- project/yahtzee/frontend/models.py
+++ project/yahtzee/frontend/models.py
@@ -1,3 +1,9 @@
+import uuid
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Game(models.Model):
+    game_id = models.UUIDField(default=uuid.uuid4, editable=False)
+    host = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    is_public = models.BooleanField()
+    join_code = models.CharField(max_length=100, null=True)
