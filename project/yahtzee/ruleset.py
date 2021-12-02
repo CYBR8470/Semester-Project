0394621ebb7b5ff29565b5baee69206aa49b8208
abullockuno <43621048+abullockuno@users.noreply.github.com>
2021-12-02 10:23:01 -0600

    Update ruleset.py
    added ruleset for upper portion

--- project/yahtzee/ruleset.py
+++ project/yahtzee/ruleset.py
@@ -1,1 +1,15 @@
 
+#Yahtzee has two sections, an upper section and a lower section.
+
+#Upper section only counts dice that match the descriptor or are zero
+
+#Given an array of dice values and a desired pip value, returns sum of all die with desired pip value
+def sumVals (dice, val):
+  sum = 0
+  for x in dice:
+    if x == val:
+      sum += val
+  return sum
+
+#Lower section has more complex rules
+#If there are three of a kind, the score returned is 
