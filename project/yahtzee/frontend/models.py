38902f83d36e599eb43d2340623f90e16befc7c2
abullockuno <43621048+abullockuno@users.noreply.github.com>
2021-12-03 16:38:29 -0600

    added hand model
    Haven't tested yet, just tried manually entering

--- project/yahtzee/frontend/models.py
+++ project/yahtzee/frontend/models.py
@@ -9,3 +9,23 @@ class Game(models.Model):
     join_code = models.CharField(max_length=100, null=True)
     active = models.BooleanField(default=True)
     is_open = models.BooleanField(default=True)
+    
+class Hand(models.Model):
+    #connect to game
+    #connect to player
+    ones = models.IntegerField(null=True)
+    twos = models.IntegerField(null=True)
+    threes = models.IntegerField(null=True)
+    fours = models.IntegerField(null=True)
+    fives = models.IntegerField(null=True)
+    sixes = models.IntegerField(null=True)
+    three_oak = models.IntegerField(null=True)
+    four_oak = models.IntegerField(null=True)
+    full_house = models.IntegerField(null=True)
+    small_straight = models.IntegerField(null=True)
+    large_straight = models.IntegerField(null=True)
+    yahtzee = models.IntegerField(null=True)
+    yahtzee_Flag = models.Boolean(default=False)
+    bonus_Yahtzees = models.IntegerField(default=0)
+    upper_bonus = models.IntegerField(default=0)
+    
